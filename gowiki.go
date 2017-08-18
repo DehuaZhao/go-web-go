bee406bb1d3fbad5cf66559f39c06737527fc59e
DehuaZhao <edwdzhao@gmail.com>
2017-08-18 22:11:08 +0800

    Step-1 Error Handling
    - Modify the `loadPage` to let it return an error if `ReadFile` encounters one.

--- gowiki.go
+++ gowiki.go
@@ -16,15 +16,18 @@ func (p *Page) save() error {
 	return ioutil.WriteFile(filename, p.Body, 0600)
 }
 
-func loadPage(title string) *Page {
+func loadPage(title string) (*Page, error) {
 	filename := title + ".txt"
-	body, _ := ioutil.ReadFile(filename)
-	return &Page{Title: title, Body: body}
+	body, err := ioutil.ReadFile(filename)
+	if err != nil {
+		return nil, err
+	}
+	return &Page{Title: title, Body: body}, nil
 }
 
 func main() {
 	p1 := &Page{Title: "TestPage", Body: []byte("This is a sample Page.")}
 	p1.save()
-	p2 := loadPage("TestPage")
+	p2, _ := loadPage("TestPage")
 	fmt.Println(string(p2.Body))
 }
